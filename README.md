1de8324fe9dbb843a518429242aa0c30fed0a345
Brice Wilson <brice@5wilsons.net>
2023-07-07 14:45:39 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# TypeScript in 4 Weeks
+
+This is the official repository for my O'Reilly course titled *TypeScript in 4 Weeks*. 
---
+++ enums.ts
@@ -0,0 +1,18 @@
+enum AppStatus {
+  NotStarted,
+  InDevelopment,
+  Deployed
+}
+
+enum AppEnvironments {
+  Dev = "Development",
+  Test = "Test",
+  Prod = "Production"
+}
+
+let currentApp = {
+  Status: AppStatus.Deployed,
+  Environment: AppEnvironments.Prod
+};
+
+console.log(currentApp);
