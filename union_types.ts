67f9aa66ae4eaf7a7bf268e1a46eafad430023f2
Brice Wilson <brice@5wilsons.net>
2023-07-07 15:36:16 -0400

    added comments

--- union_types.ts
+++ union_types.ts
@@ -1,7 +1,7 @@
-let my_union_var: number | string;
+let my_union_var: number | string; // union type with number and string
 
 my_union_var = 10;
 my_union_var = "testing";
 // my_union_var = true; // error TS2322: Type 'boolean' is not assignable to type 'string | number'.
 
-let my_intersection_var: number & string;
+let my_intersection_var: number & string; // intersection type with number and string
