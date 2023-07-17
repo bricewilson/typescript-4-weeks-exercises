90e96c48bd51404eee8f1fbfc5ba8d6882b064c8
Brice Wilson <brice@5wilsons.net>
2023-07-17 18:28:56 -0400

    modules

---
+++ salary.ts
@@ -0,0 +1,5 @@
+import { IncreaseSalary } from "./utility_functions";
+
+let newSalary: number = IncreaseSalary(55000, .25);
+
+console.log(newSalary);
---
+++ utility_functions.ts
@@ -0,0 +1,3 @@
+export function IncreaseSalary(currentSalary: number, increase: number): number {
+  return currentSalary * (1 + increase);
+}
